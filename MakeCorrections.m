d6e6d38f0d7a038825317aa942f661498f32fe61
AirinGuz <169070531+AirinGuz@users.noreply.github.com>
2024-05-06 17:00:44 -0500

    ds

--- MakeCorrections.m
+++ MakeCorrections.m
@@ -1,5 +1,5 @@
 wavelenght=[443; 490; 531; 565; 610; 665; 705; 865];
-hcube = hypercube("2023_16bits.tif", wavelenght);
+hcube = hypercube("20201013_143255_48_2277_3B_AnalyticMS_SR_8b_clip.tif", wavelenght);
 rgbImg = colorize(hcube,'Method','RGB');
 figure
 imagesc(rgbImg)
